99d4b661b25c344f4c68954920cddb717aaed8f8
yq <yq_xx@yahoo.com>
2017-06-06 17:04:49 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var request = require('request');
+
+var app = express();
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'bower_components')));
+
+app.get('/', function(req, res) {
+	request('https://api.liyiqi.me/list', function(error, response, body) {
+		res.render('index', {
+			title: 'test index',
+			items: JSON.parse(body)
+		});
+	});
+});
+
+app.listen(3000);
+console.log('Express started on port 3000');
