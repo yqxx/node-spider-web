27b2693d9395978dad55594f6be4d1593a1369d4
yq <yq_xx@yahoo.com>
2017-06-09 15:54:53 +0800

    v1.0

---
+++ static/js/main.js
@@ -0,0 +1,22 @@
+(function($){
+	var proxy = $.proxy;
+
+    Page = {
+        init: function(){
+			var that = this;
+			
+			$("#back-to-top").on("click",proxy(that.onTopClick,that));
+			$(window).scroll(function() {
+				$(this).scrollTop() > 100 ? $("#back-to-top").fadeIn() : $("#back-to-top").fadeOut()
+			})
+        },
+		onTopClick: function(e) {
+			return e.preventDefault(),
+				$("html, body").animate({
+					scrollTop: 0
+				}, 1000), false
+		}
+    };
+	
+    Page.init();
+})(jQuery);
